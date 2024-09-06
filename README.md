979b3f485ff5eb3c605da02b104144ee4f72f7c1
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-09-06 19:15:43 +0900

    [추가] 프로젝트 생성  - devlop 타겟 및 info.plist 추가  - gitignore 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sooum-iOS
+
+Sooum iOS 클라이언트 통합 프로젝트 입니다.
---
+++ SOOUM/SOOUM/Presentations/TempViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOOUM
+//
+//  Created by 오현식 on 9/3/24.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
