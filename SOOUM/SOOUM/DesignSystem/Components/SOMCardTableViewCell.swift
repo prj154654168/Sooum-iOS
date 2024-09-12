c5bf7ab73e2f86c586cb72c39bd1982a5022b251
JDeoks <duginee@naver.com>
2024-09-12 21:48:03 +0900

    [추가] SOMCardTableViewCell 추가

---
+++ SOOUM/SOOUM/DesignSystem/Components/SOMCardTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SOMCardTableViewCell.swift
+//  SOOUM
+//
+//  Created by JDeoks on 9/12/24.
+//
+
+import UIKit
+
+class SOMCardTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    }
+
+}
