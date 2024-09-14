67145e9ebe267c1266d1f9fd230b3a19f50c4faa
JDeoks <duginee@naver.com>
2024-09-15 02:52:47 +0900

    [추가] 카드 펑 기능 추가

--- SOOUM/SOOUM/DesignSystem/Components/SOMCardTableViewCell.swift
+++ SOOUM/SOOUM/DesignSystem/Components/SOMCardTableViewCell.swift
@@ -5,14 +5,14 @@
 //  Created by JDeoks on 9/12/24.
 //
 
+import RxSwift
 import SnapKit
 import Then
 import UIKit
 
 class SOMCardTableViewCell: UITableViewCell {
-//    모델은 대충 만들기
-//    펑 구현 인터벌 사용
     
+    // TODO: - 모드 구현
     /// homeSelect 값에 따라 스택뷰 순서 변함
     enum Mode {
         case latest
@@ -20,27 +20,61 @@ class SOMCardTableViewCell: UITableViewCell {
         case distance
     }
     
+    let id = Int.random(in: 0...200)
+    
+    var card: Card?
+    
     /// 카드 펑 타임
     var pungTime: Date?
     
+    /// 셀 카드 펑 이벤트
+    let didpung = PublishSubject<Void>()
+    
+    var disposeBag = DisposeBag()
+
     let cardView = SOMCard()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        initView()
+        initUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - setData
+    func setData(card: Card) {
+        self.pungTime = card.pungTime
+        self.card = card
+        self.cardView.cardTextContentLabel.text = "\(card.id)"
+        self.cardView.cardPungTimeLabel.text = getTimeOutStr(pungTime: card.pungTime)
+        self.subscribePungTime()
+    }
+    
+    /// 남은 시간 스트링으로 반환
+    private func getTimeOutStr(pungTime: Date) -> String {
+        let remainingTime = Int(pungTime.timeIntervalSince(Date()))
 
-    // MARK: - initView
-    private func initView() {
+        if remainingTime <= 0 {
+            return "00:00:00"
+        }
+        
+        let hours = remainingTime / 3600
+        let minutes = (remainingTime % 3600) / 60
+        let seconds = remainingTime % 60
+        
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+
+    // MARK: - initUI
+    private func initUI() {
         self.selectionStyle = .none
         addSubviews()
         initConstraint()
     }
     
+    // MARK: - addSubviews
     private func addSubviews() {
         contentView.addSubview(cardView)
     }
@@ -53,5 +87,23 @@ class SOMCardTableViewCell: UITableViewCell {
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
+    }
+    
+    private func subscribePungTime() {
+        // 매 1초마다 펑 여부 확인
+//        intervalDisposable = 
+        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self, let pungTime = self.pungTime else {
+                    return
+                }
+                let remainingTime = pungTime.timeIntervalSince(Date())
+                if remainingTime > 0 {
+                    self.cardView.cardPungTimeLabel.text = getTimeOutStr(pungTime: pungTime)
+                } else {
+                    self.didpung.onNext(())
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
