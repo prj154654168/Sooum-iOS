8192ada37c13fabd2303befed82d8f17838276a7
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-10-01 14:33:06 +0900

    [변경] 이미지 로딩 전 화면 변경
     - placeholder 색상 변경 white -> gray02
     - Loading label 추가

--- SOOUM/SOOUM/Extensions/Cocoa/UIImgeView.swift
+++ SOOUM/SOOUM/Extensions/Cocoa/UIImgeView.swift
@@ -12,21 +12,45 @@ import Kingfisher
 
 extension UIImageView {
 
-    static let placeholder: UIImage? = UIColor.som.white.toImage
+    static let placeholder: UIImage? = UIColor.som.gray02.toImage
 
     func setImage(strUrl: String?) {
+        /// Placeholder view
+        self.image = Self.placeholder
+        self.backgroundColor = .clear
+        
+        var label: UILabel {
+            let label = UILabel()
+            label.text = "Loading..."
+            label.textColor = .som.white
+            label.typography = .init(
+                fontContainer: Pretendard(size: 18, weight: .medium),
+                lineHeight: 21.48,
+                letterSpacing: -0.04
+            )
+            return label
+        }
+        
+        self.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.centerYAnchor)
 
+        ])
+        /// Image load
         if let strUrl: String = strUrl, let url = URL(string: strUrl) {
             let header = AnyModifier { request in
                 var req = request
                 req.addValue("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjY5Mjk0MzMsImV4cCI6MTAxNzI2OTI5NDMzLCJzdWIiOiJBY2Nlc3NUb2tlbiIsImlkIjo2MjUwNDc5NzMyNTA1MTUxNTMsInJvbGUiOiJVU0VSIn0.aL4Tr3FaSwvu9hOQISAvGJfCHBGCV9jRo_BfTQkBssU", forHTTPHeaderField: "Authorization")
                 return req
             }
-            self.kf.setImage(with: url, options: [.requestModifier(header)])
-            self.backgroundColor = .clear
+            self.kf.setImage(with: url, options: [.requestModifier(header)]) { _ in
+                /// Delete label if image loading is successful
+                label.removeFromSuperview()
+            }
         } else {
             self.kf.cancelDownloadTask()
-            self.image = Self.placeholder
         }
     }
 }
