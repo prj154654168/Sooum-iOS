7a8bf8190043ba5cd85f676a8faecdfbf1d318cf
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-10-21 14:04:10 +0900

    [변경] 전체적으로 사용될 Status와 EmptyInitializable 모델 폴더 위치 변경

--- SOOUM/SOOUM/Models/Card/CardProtocol.swift
+++ SOOUM/SOOUM/Models/Card/CardProtocol.swift
@@ -8,26 +8,6 @@
 import Foundation
 
 
-/// 서버 응답 코드가 204 or response.data == nil일 때
-protocol EmptyInitializable {
-    static func empty() -> Self
-}
-
-/// 서버 응답 status
-struct Status: Codable {
-    let httpCode: Int
-    let httpStatus: String
-    let responseMessage: String
-}
-
-extension Status {
-    init() {
-        self.httpCode = 0
-        self.httpStatus = ""
-        self.responseMessage = ""
-    }
-}
-
 protocol CardProtocol: Equatable, Codable {
     var id: String { get }
     var content: String { get }
