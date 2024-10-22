05d65c2215577211ed2240ea911698e713cef98e
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-10-22 20:15:23 +0900

    [추가] 서버 요청 응답에 따라 관련 태그 업데이트

--- SOOUM/SOOUM/Presentations/Main/WriteCard/WriteCardViewReactor.swift
+++ SOOUM/SOOUM/Presentations/Main/WriteCard/WriteCardViewReactor.swift
@@ -11,21 +11,46 @@ import ReactorKit
 class WriteCardViewReactor: Reactor {
     
     enum Action: Equatable {
-        case refresh
         case writeCard
-        case relatedTags(String)
+        case relatedTags(keyword: String)
     }
     
     enum Mutation {
-        case updateCard
-        case updateRelatedTags
+        case relatedTags([RelatedTag])
     }
     
-    struct State { }
-    
-    var initialState: State {
-        .init()
+    struct State {
+        var relatedTags: [RelatedTag]
     }
+    
+    var initialState: State = .init(
+        relatedTags: []
+    )
+    
+    private let networkManager = NetworkManager.shared
     
     init() { }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .writeCard:
+            return .empty()
+        case .relatedTags(let keyword):
+            
+            let request: CardRequest = .relatedTag(keyword: keyword, size: 5)
+            
+            return self.networkManager.request(RelatedTagResponse.self, request: request)
+                .map(\.embedded.relatedTags)
+                .map { .relatedTags($0) }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state: State = state
+        switch mutation {
+        case let .relatedTags(relatedTags):
+            state.relatedTags = relatedTags
+        }
+        return state
+    }
 }
