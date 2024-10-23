95034b56297ded4e96bd560dbdf547fc28e19a85
JDeoks <duginee@naver.com>
2024-10-23 23:40:45 +0900

    [수정] UploadCardBottomSheetViewController - segmentState BehaviorRelay로 수정

--- SOOUM/SOOUM/Presentations/UploadCard/UpoadCardBottomSheet/UploadCardBottomSheetViewReactor.swift
+++ SOOUM/SOOUM/Presentations/UploadCard/UpoadCardBottomSheet/UploadCardBottomSheetViewReactor.swift
@@ -5,29 +5,36 @@
 //  Created by JDeoks on 10/16/24.
 //
 
+import UIKit
+
 import ReactorKit
 
  class UploadCardBottomSheetViewReactor: Reactor {
+     
+     struct ImageWithName {
+         var name: String
+         var image: UIImage
+     }
+     
      enum Action: Equatable {
-         case refresh
-         case coordinate(String, String)
+         /// 이미지 변경 눌렀을때 호출
+         case fetchNewDefaultImage
+         /// 이미지 업로드용 url, 이름 fetch후 이미지 업로드 까지 이미지 선택 완료시 호출
+         case seleteMyImage(UIImage)
      }
      
      enum Mutation {
-         case detailCard(DetailCard)
-         case updateCoordinate(String, String)
-         case updateIsLoading(Bool)
+         /// 기본 이미지 fetch 결과
+         case defaultImage(ImageWithName)
+         /// 내 이미지 이름 fetch, 이미지 put 까지 완료 하고 이미지 이름 반환
+         case myImageName(String)
      }
      
      struct State {
-         var detailCard: DetailCard
-         var coordinate: (String?, String?)
-         var isLoading: Bool
+         var defaultImages: [ImageWithName]
      }
      
      var initialState: State = .init(
-         detailCard: .init(),
-         coordinate: (nil, nil),
-         isLoading: false
+        defaultImages: []
      )
  }
