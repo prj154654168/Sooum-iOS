718f35612ee6a9def48905019826ec17d66a330e
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-11-01 19:30:23 +0900

    [추가] 피드 작성 api 가이드 추가

--- SOOUM/SOOUM/Presentations/Main/WriteCard/WriteCardViewReactor.swift
+++ SOOUM/SOOUM/Presentations/Main/WriteCard/WriteCardViewReactor.swift
@@ -11,34 +11,53 @@ import ReactorKit
 class WriteCardViewReactor: Reactor {
     
     enum Action: Equatable {
-        case writeCard
+        case writeCard(
+            isPublic: Bool,
+            isStory: Bool,
+            content: String,
+            font: String,
+            imgType: String,
+            imgName: String,
+            feedTags: [String]
+        )
         case relatedTags(keyword: String)
     }
     
     enum Mutation {
+        case writeCard(Bool)
         case relatedTags([RelatedTag])
     }
     
     struct State {
+        var isWrite: Bool
         var relatedTags: [RelatedTag]
     }
     
     var initialState: State = .init(
+        isWrite: false,
         relatedTags: []
     )
     
     private let networkManager = NetworkManager.shared
+    private let locationManager = LocationManager.shared
     
     init() { }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .writeCard:
+        case let .writeCard(
+            isPublic,
+            isStory,
+            content,
+            font,
+            imgType,
+            imgName,
+            feedTags
+        ):
             return .empty()
-        case .relatedTags(let keyword):
+        case let .relatedTags(keyword):
             
             let request: CardRequest = .relatedTag(keyword: keyword, size: 5)
-            
             return self.networkManager.request(RelatedTagResponse.self, request: request)
                 .map(\.embedded.relatedTags)
                 .map { .relatedTags($0) }
@@ -48,6 +67,8 @@ class WriteCardViewReactor: Reactor {
     func reduce(state: State, mutation: Mutation) -> State {
         var state: State = state
         switch mutation {
+        case let .writeCard(isWrite):
+            state.isWrite = isWrite
         case let .relatedTags(relatedTags):
             state.relatedTags = relatedTags
         }
