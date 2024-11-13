e5b6dc20c6306e7b4c9e6db2f1e2cf26b3b45b77
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-11-13 15:52:21 +0900

    [변경] homeTabBar 및 LocationFilter 델리게이트 코드 이전

--- SOOUM/SOOUM/Presentations/Main/Home/Views/MainHomeHeaderView.swift
+++ SOOUM/SOOUM/Presentations/Main/Home/Views/MainHomeHeaderView.swift
@@ -18,9 +18,6 @@ class MainHomeHeaderView: UIView {
     
     private let height = SOMHomeTabBar.height + 54 + 2
     
-    let homeTabBarDidTap = PublishRelay<Int>()
-    let locationFilterDidTap = PublishRelay<String>()
-    
     let container = UIStackView().then {
         $0.axis = .vertical
         $0.alignment = .fill
@@ -28,13 +25,9 @@ class MainHomeHeaderView: UIView {
         $0.spacing = 2
     }
     
-    lazy var homeTabBar = SOMHomeTabBar().then {
-        $0.delegate = self
-    }
+    let homeTabBar = SOMHomeTabBar()
     
-    lazy var locationFilter = SOMLocationFilter().then {
-        $0.delegate = self
-    }
+    let locationFilter = SOMLocationFilter()
     
     var isLocationFilterHidden: Bool {
         set { self.locationFilter.isHidden = newValue }
@@ -47,9 +40,8 @@ class MainHomeHeaderView: UIView {
         super.init(frame: .zero)
         
         self.setupConstraints()
-        /// 시작 인덱스
-        self.homeTabBar.didSelectTab(0)
-        /// locationFilter hidden 에 따른 높이
+        
+        // locationFilter hidden 에 따른 높이
         self.locationFilter.rx.observe(Bool.self, "hidden")
             .distinctUntilChanged()
             .subscribe(with: self) { object, isHidden in
@@ -91,26 +83,5 @@ class MainHomeHeaderView: UIView {
                 self.height
             $0.height.equalTo(height)
         }
-    }
-}
-
-
-// MARK: MainHomeHeaderView Delegate
-
-extension MainHomeHeaderView: SOMHomeTabBarDelegate {
-    
-    func tabBar(_ tabBar: SOMHomeTabBar, didSelectTabAt index: Int) {
-        self.homeTabBarDidTap.accept(index)
-        self.isLocationFilterHidden = index != 2
-    }
-}
-
-extension MainHomeHeaderView: SOMLocationFilterDelegate {
-    
-    func filter(
-        _ filter: SOMLocationFilter,
-        didSelectDistanceAt distance: SOMLocationFilter.Distance
-    ) {
-        self.locationFilterDidTap.accept(distance.rawValue)
     }
 }
