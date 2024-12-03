51d9265169a672924da45aa17c8a729573fefe71
JDeoks <duginee@naver.com>
2024-12-04 02:32:47 +0900

    [추가] TagRequest 추가

---
+++ SOOUM/SOOUM/Resources/Alamofire/Develop/TagRequest.swift
@@ -0,0 +1,87 @@
+//
+//  TagRequest.swift
+//  SOOUM
+//
+//  Created by JDeoks on 12/4/24.
+//
+
+import Foundation
+
+import Alamofire
+
+enum TagRequest: BaseRequest {
+    case favorite(last: String?)
+    case recommend
+    
+    var path: String {
+        switch self {
+        case let .favorite(last):
+            if let last = last {
+                return "/tags/favorites/\(last)"
+            } else {
+                return "/tags/favorites"
+            }
+        case .recommend:
+            return "/imgs/cards/upload"
+        }
+    }
+        
+    var method: HTTPMethod {
+        switch self {
+        case .favorite, .recommend:
+            return .get
+        }
+    }
+    
+    var parameters: Parameters {
+        switch self {
+        case .favorite, .recommend:
+            return [:]
+        }
+    }
+    
+    var encoding: ParameterEncoding {
+        switch self {
+        case .favorite, .recommend:
+            return URLEncoding.queryString
+        }
+    }
+    
+    var authorizationType: AuthorizationType {
+        switch self {
+        case .favorite, .recommend:
+                .access
+        }
+    }
+        
+    func asURLRequest() throws -> URLRequest {
+
+        if let url = URL(string: Constants.endpoint)?.appendingPathComponent(self.path) {
+            var request = URLRequest(url: url)
+            request.method = self.method
+            
+            switch self.authorizationType {
+            case .access:
+                let authPayload = AuthManager.shared.authPayloadByAccess()
+                let authKey = authPayload.keys.first! as String
+                request.setValue(authPayload[authKey], forHTTPHeaderField: authKey)
+            default:
+                break
+            }
+            
+            request.setValue(
+                Constants.ContentType.json.rawValue,
+                forHTTPHeaderField: Constants.HTTPHeader.contentType.rawValue
+            )
+            request.setValue(
+                Constants.ContentType.json.rawValue,
+                forHTTPHeaderField: Constants.HTTPHeader.acceptType.rawValue
+            )
+
+            let encoded = try encoding.encode(request, with: self.parameters)
+            return encoded
+        } else {
+            return URLRequest(url: URL(string: "")!)
+        }
+    }
+}
