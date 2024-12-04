6cb700fdb6ac9b7b6a0e68d045e61d143e125bf0
JDeoks <duginee@naver.com>
2024-12-04 21:31:38 +0900

    [추가] SearchTagsResponse 추가

---
+++ SOOUM/SOOUM/Models/Tags/SearchTagsResponse.swift
@@ -0,0 +1,39 @@
+//
+//  SearchTagsResponse.swift
+//  SOOUM
+//
+//  Created by JDeoks on 12/4/24.
+//
+
+import Foundation
+
+// MARK: - SearchTagsResponse
+struct SearchTagsResponse: Codable {
+    
+    // MARK: - Embedded
+    struct Embedded: Codable {
+        let relatedTagList: [RelatedTag]
+    }
+
+    // MARK: - RelatedTagList
+    struct RelatedTag: Codable {
+        let count: Int
+        let content: String
+    }
+
+    // MARK: - Status
+    struct Status: Codable {
+        let httpCode: Int
+        let httpStatus, responseMessage: String
+    }
+    
+    let embedded: Embedded
+    let status: Status
+
+    enum CodingKeys: String, CodingKey {
+        case embedded = "_embedded"
+        case status
+    }
+}
+
+
--- SOOUM/SOOUM/Presentations/Tags/TagSearch/TagSearchViewReactor.swift
+++ SOOUM/SOOUM/Presentations/Tags/TagSearch/TagSearchViewReactor.swift
@@ -8,49 +8,49 @@
 import ReactorKit
 
 class TagSearchViewReactor: Reactor {
-//    
-//    enum Action {
-//        case searchTag(String)
-//        case selectTag(String)
-//    }
-//    
-//    enum Mutation {
-//        /// 즐겨찾기 태그 fetch
-//        case searchTags([])
-//        case setSelectTagFinished
-//    }
-//    
-//    struct State {
-//        /// 즐겨찾기 태그 리스트
-//        fileprivate(set) var searchTags: [FavoriteTagsResponse.FavoriteTagList] = []
-//        /// 추천 태그 리스트
-//        fileprivate(set) var recommendTags: [RecommendTagsResponse.RecommendTag] = []
-//    }
-//    
-//    var initialState = State()
-//    var isFavoriteTagsEmpty: Bool {
-//        self.currentState.favoriteTags.isEmpty
-//    }
-//    
-//    func mutate(action: Action) -> Observable<Mutation> {
-//        switch action {
-//        case .fetchTags:
-//            return .concat([
-//                self.fetchFavoriteTags(),
-//                self.fetchRecommendTags()
-//            ])
-//        }
-//    }
-//    
-//    func reduce(state: State, mutation: Mutation) -> State {
-//        var newState = state
-//        switch mutation {
-//        case let .favoriteTags(favoriteTags):
-//            newState.favoriteTags = favoriteTags
-//            
-//        case let .recommendTags(recommendTags):
-//            newState.recommendTags = recommendTags
-//        }
-//        return newState
-//    }
+    
+    enum Action {
+        case searchTag(String)
+        case selectTag(String)
+    }
+    
+    enum Mutation {
+        /// 즐겨찾기 태그 fetch
+        case searchTags([])
+        case setSelectTagFinished
+    }
+    
+    struct State {
+        /// 즐겨찾기 태그 리스트
+        fileprivate(set) var searchTags: [FavoriteTagsResponse.FavoriteTagList] = []
+        /// 추천 태그 리스트
+        fileprivate(set) var recommendTags: [RecommendTagsResponse.RecommendTag] = []
+    }
+    
+    var initialState = State()
+    var isFavoriteTagsEmpty: Bool {
+        self.currentState.favoriteTags.isEmpty
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .fetchTags:
+            return .concat([
+                self.fetchFavoriteTags(),
+                self.fetchRecommendTags()
+            ])
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .favoriteTags(favoriteTags):
+            newState.favoriteTags = favoriteTags
+            
+        case let .recommendTags(recommendTags):
+            newState.recommendTags = recommendTags
+        }
+        return newState
+    }
 }
