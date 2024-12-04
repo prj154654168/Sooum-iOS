10662601c05af654f8588930a458aab9c2937071
JDeoks <duginee@naver.com>
2024-12-04 23:11:05 +0900

    [추가] TagDetailNavigationBarView 추가

--- SOOUM/SOOUM/Presentations/Tags/TagDetail/TagDetailViewController.swift
+++ SOOUM/SOOUM/Presentations/Tags/TagDetail/TagDetailViewController.swift
@@ -5,8 +5,58 @@
 //  Created by JDeoks on 12/4/24.
 //
 
-import Foundation
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxGesture
+import RxSwift
+
+import SnapKit
+import Then
 
 class TagDetailViewController: BaseViewController {
+    
+    let navBarView = TagDetailNavigationBarView()
+    
+    lazy var tableView = UITableView(frame: .zero, style: .plain).then {
+        $0.backgroundColor = .clear
+        $0.indicatorStyle = .black
+        $0.separatorStyle = .none
+        
+        $0.register(
+            MainHomeViewCell.self,
+            forCellReuseIdentifier: String(describing:  MainHomeViewCell.self)
+        )
+        $0.dataSource = self
+        $0.delegate = self
+    }
+    
+    override func setupConstraints() {
+        <#code#>
+    }
+}
+
+extension TagDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+//        let model = SOMCardModel(data: self.cards[indexPath.row])
+        
+        let cell: MainHomeViewCell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing:  MainHomeViewCell.self),
+            for: indexPath
+        ) as! MainHomeViewCell
+//        cell.selectionStyle = .none
+//        cell.setModel(model)
+        // 카드 하단 contents 스택 순서 변경
+//        cell.changeOrderInCardContentStack(self.reactor?.currentState.selectedIndex ?? 0)
+        
+        return cell
+    }
     
 }
