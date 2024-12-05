c89055bd515771db73fdce850610995921d8ff4a
JDeoks <duginee@naver.com>
2024-12-05 20:52:03 +0900

    [추가] TagDetailCardResponse 추가

---
+++ SOOUM/SOOUM/Models/Tags/TagInfoResponse.swift
@@ -0,0 +1,23 @@
+//
+//  TagInfoResponse.swift
+//  SOOUM
+//
+//  Created by JDeoks on 12/5/24.
+//
+
+import Foundation
+
+// MARK: - TagInfoResponse
+struct TagDetailCardResponse: Codable {
+    
+    // MARK: - Status
+    struct Status: Codable {
+        let httpCode: Int
+        let httpStatus, responseMessage: String
+    }
+    
+    let content: String
+    let cardCnt: Int
+    let isFavorite: Bool
+    let status: Status
+}
