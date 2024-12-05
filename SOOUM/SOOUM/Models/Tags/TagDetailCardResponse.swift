df4c5c8233b6e0460a7ef889a5b18c321bae1b90
JDeoks <duginee@naver.com>
2024-12-05 19:39:04 +0900

    [추가] TagDetailCardResponse 추가

---
+++ SOOUM/SOOUM/Models/Tags/TagDetailCardResponse.swift
@@ -0,0 +1,78 @@
+//
+//  TagDetailCardResponse.swift
+//  SOOUM
+//
+//  Created by JDeoks on 12/4/24.
+//
+
+import Foundation
+
+// MARK: - TagDetailCardResponse
+struct TagDetailCardResponse: Codable {
+    
+    // MARK: - Embedded
+    struct Embedded: Codable {
+        let tagFeedCardDtoList: [TagFeedCardDtoList]
+    }
+
+    // MARK: - TagFeedCardDtoList
+    struct TagFeedCardDtoList: Codable {
+        let id, content, createdAt: String
+        let likeCnt, commentCnt: Int
+        let backgroundImgURL: Next
+        let font: Font
+        let fontSize: FontSize
+        let distance: Double?
+        let links: TagFeedCardDtoListLinks
+        let isLiked, isCommentWritten: Bool
+
+        enum CodingKeys: String, CodingKey {
+            case id, content, createdAt, likeCnt, commentCnt
+            case backgroundImgURL = "backgroundImgUrl"
+            case font, fontSize, distance
+            case links = "_links"
+            case isLiked, isCommentWritten
+        }
+    }
+
+    // MARK: - Next
+    struct Next: Codable {
+        let href: String
+    }
+
+    enum Font: String, Codable {
+        case pretendard = "PRETENDARD"
+    }
+
+    enum FontSize: String, Codable {
+        case big = "BIG"
+    }
+
+    // MARK: - TagFeedCardDtoListLinks
+    struct TagFeedCardDtoListLinks: Codable {
+        let detail: Next
+    }
+
+    // MARK: - TagDetailCardResponseLinks
+    struct TagDetailCardResponseLinks: Codable {
+        let next: Next
+    }
+
+    // MARK: - Status
+    struct Status: Codable {
+        let httpCode: Int
+        let httpStatus, responseMessage: String
+    }
+    
+    let embedded: Embedded
+    let links: TagDetailCardResponseLinks
+    let status: Status
+
+    enum CodingKeys: String, CodingKey {
+        case embedded = "_embedded"
+        case links = "_links"
+        case status
+    }
+}
+
+
