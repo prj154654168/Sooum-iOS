07a11e163b1ae12422d44e88d0b624e20608aa93
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-12-10 23:34:33 +0900

    [수정] SOMCard pungTime 수정
     - NetworkManager DateFormatter 에 korea 설정
     - Date 확장에 pungTime 포맷 추가
     - SOMCard 재사용 시 레이아웃 및 데이터 초기화 추가
     - SOMCard 타이머 로직 수정

--- SOOUM/SOOUM/Extensions/Foundation/Date.swift
+++ SOOUM/SOOUM/Extensions/Foundation/Date.swift
@@ -67,6 +67,23 @@ extension Date {
         return ""
     }
     
+    func infoReadableTimeTakenFromThisForPung(to: Date) -> String {
+        
+        let from: TimeInterval = self.timeIntervalSince1970
+        let to: TimeInterval = to.timeIntervalSince1970
+        let gap: TimeInterval = max(0, to - from)
+
+        let time: Int = .init(gap)
+        let hours: Int = .init(time) / (60 * 60)
+        let minutes: Int = .init(time % (60 * 60)) / 60
+        let seconds: Int = .init(time % 60)
+        
+        if hours <= 0 && minutes <= 0 && seconds <= 0 {
+            return "00 : 00 : 00"
+        }
+        return String(format: "%02d : %02d : %02d", hours, minutes, seconds)
+    }
+    
     var banEndFormatted: String {
         return self.toString("yyyy년 MM월 dd일")
     }
