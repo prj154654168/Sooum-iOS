160b2e01e3bf49f7077330e96c7aeaf543f79c66
JDeoks <duginee@naver.com>
2024-12-19 22:01:41 +0900

    [추가] EmptyTagDetailTableViewCell setData 추가

--- SOOUM/SOOUM/Presentations/Tags/TagDetail/Cells/EmptyTagDetailTableViewCell.swift
+++ SOOUM/SOOUM/Presentations/Tags/TagDetail/Cells/EmptyTagDetailTableViewCell.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class EmptyTagDetailTableViewCell: UITableViewCell {
   
+  enum Mode {
+    case noCardsCanView
+    case noCardsRegistered
+    
+    var title: String {
+      switch self {
+      case .noCardsCanView:
+        "조회할 수 있는 카드가 없어요"
+      case .noCardsRegistered:
+        "등록된 카드가 없어요"
+      }
+    }
+    
+    var desc: String {
+      switch self {
+      case .noCardsCanView:
+        "차단된 사용자의 카드는\n확인할 수 없어요"
+      case .noCardsRegistered:
+        "해당 태그를 사용한\n카드가 없어요"
+      }
+    }
+  }
+  
   let stackView = UIStackView().then {
     $0.axis = .vertical
     $0.spacing = 12
@@ -35,6 +58,11 @@ class EmptyTagDetailTableViewCell: UITableViewCell {
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setData(mode: Mode) {
+    titleLabel.text = mode.title
+    descLabel.text = mode.desc
   }
   
   // MARK: - setupConstraint
