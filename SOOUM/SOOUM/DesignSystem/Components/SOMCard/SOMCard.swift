240210b435cec0a3ac578256cd837acca1abdd1e
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-12-20 17:10:17 +0900

    [수정] 피드 카드 시간 표시 수정
     - 한국시간으로 변경 후 TimeInterval 계산

--- SOOUM/SOOUM/DesignSystem/Components/SOMCard/SOMCard.swift
+++ SOOUM/SOOUM/DesignSystem/Components/SOMCard/SOMCard.swift
@@ -358,7 +358,7 @@ class SOMCard: UIView {
             .init(.icon(.outlined(.comment)))
         commentImageView.tintColor = model.data.isCommentWritten ? .som.p300 : .som.white
         
-        timeLabel.text = model.data.createdAt.infoReadableTimeTakenFromThis(to: Date().toKorea())
+        timeLabel.text = model.data.createdAt.toKorea().infoReadableTimeTakenFromThis(to: Date().toKorea())
         distanceInfoStackView.isHidden = model.data.distance == nil
         distanceLabel.text = (model.data.distance ?? 0).infoReadableDistanceRangeFromThis()
         likeLabel.text = model.data.likeCnt > 99 ? "99+" : "\(model.data.likeCnt)"
@@ -388,7 +388,7 @@ class SOMCard: UIView {
             .init(.icon(.outlined(.comment)))
         commentImageView.tintColor = tagCard.isCommentWritten ? .som.p300 : .som.white
         
-        timeLabel.text = tagCard.createdAt.infoReadableTimeTakenFromThis(to: Date().toKorea())
+        timeLabel.text = tagCard.createdAt.toKorea().infoReadableTimeTakenFromThis(to: Date().toKorea())
         distanceInfoStackView.isHidden = tagCard.distance == nil
         distanceLabel.text = (tagCard.distance ?? 0).infoReadableDistanceRangeFromThis()
         likeLabel.text = "\(tagCard.likeCnt)"
