af61c7a37e85de8298bf92880c5ba4e422c34d6d
hyeonsik971029 <hyeonsik971029@gmail.com>
2024-12-31 10:59:00 +0900

    [추가] 메인 탭바 진입 유형 추가
     - 진입 유형에 따라 네비게이션 하는 화면 분기
     - 푸시 알림을 통해 진입 시 해당 알림 읽음 처리

--- SOOUM/SOOUM/Presentations/Main/Home/MainHomeTabBarReactor.swift
+++ SOOUM/SOOUM/Presentations/Main/Home/MainHomeTabBarReactor.swift
@@ -7,23 +7,31 @@
 
 import ReactorKit
 
+import Alamofire
+
 
 class MainHomeTabBarReactor: Reactor {
 
     enum Action: Equatable {
         case notisWithoutRead
+        case requestRead(String)
     }
     
     
     enum Mutation {
         case notisWithoutRead(Bool)
+        case updateIsReadCompleted(Bool)
     }
     
     struct State {
         var isNotisWithoutRead: Bool
+        var isReadCompleted: Bool
     }
     
-    var initialState: State = .init(isNotisWithoutRead: false)
+    var initialState: State = .init(
+        isNotisWithoutRead: false,
+        isReadCompleted: false
+    )
     
     private let networkManager = NetworkManager.shared
     let locationManager = LocationManager.shared
@@ -36,6 +44,10 @@ class MainHomeTabBarReactor: Reactor {
             return self.networkManager.request(CommentHistoryInNotiResponse.self, request: request)
                 .map { $0.commentHistoryInNotis.isEmpty }
                 .map(Mutation.notisWithoutRead)
+        case let .requestRead(selectedId):
+            let request: NotificationRequest = .requestRead(notificationId: selectedId)
+            return self.networkManager.request(Empty.self, request: request)
+                .map { _ in .updateIsReadCompleted(true) }
         }
     }
     
@@ -44,6 +56,8 @@ class MainHomeTabBarReactor: Reactor {
         switch mutation {
         case let .notisWithoutRead(isNotisWithoutRead):
             state.isNotisWithoutRead = isNotisWithoutRead
+        case let .updateIsReadCompleted(isReadCompleted):
+            state.isReadCompleted = isReadCompleted
         }
         return state
     }
