464d1568c0cffcbdcfd61b91fc1eef1e838c0ae0
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-01-16 14:53:03 +0900

    [변경] 인증 토큰 interceptor에서 추가 및 interceptor 추가 방식 변경

---
+++ SOOUM/SOOUM/Managers/NetworkManager/Interceptor/AddingTokenInterceptor.swift
@@ -0,0 +1,40 @@
+//
+//  AddingTokenInterceptor.swift
+//  SOOUM
+//
+//  Created by 오현식 on 1/15/25.
+//
+
+import Foundation
+
+import Alamofire
+
+
+class AddingTokenInterceptor: RequestInterceptor {
+    
+    private let provider: ManagerProviderType
+    
+    init(provider: ManagerProviderType) {
+        self.provider = provider
+    }
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, any Error>) -> Void) {
+        var request = urlRequest
+        let authorizationType = request.value(forHTTPHeaderField: "AuthorizationType")
+        
+        switch authorizationType {
+        case "access":
+            let authPayloadForAccess = self.provider.authManager.authPayloadByAccess()
+            let authKeyForAccess = authPayloadForAccess.keys.first! as String
+            request.setValue(authPayloadForAccess[authKeyForAccess], forHTTPHeaderField: authKeyForAccess)
+        case "refresh":
+            let authPayloadForRefresh = self.provider.authManager.authPayloadByRefresh()
+            let authKeyForRefresh = authPayloadForRefresh.keys.first! as String
+            request.setValue(authPayloadForRefresh[authKeyForRefresh], forHTTPHeaderField: authKeyForRefresh)
+        default:
+            break
+        }
+        
+        completion(.success(request))
+    }
+}
