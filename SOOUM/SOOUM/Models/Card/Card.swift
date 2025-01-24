01043227821c48a370695502d0d42c9dae4e47d2
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-01-24 16:59:30 +0900

    [추가] Card 모델 비교 시 isLiked도 비교하는 것으로 추가

--- SOOUM/SOOUM/Models/Card/Card.swift
+++ SOOUM/SOOUM/Models/Card/Card.swift
@@ -49,7 +49,10 @@ struct Card: CardProtocol {
 extension Card {
     // Card의 == 조건 id 및 좋아요 수, 코멘트 수
     static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id && lhs.likeCnt == rhs.likeCnt && lhs.isCommentWritten == rhs.isCommentWritten
+        lhs.id == rhs.id &&
+        lhs.isLiked == rhs.isLiked &&
+        lhs.likeCnt == rhs.likeCnt &&
+        lhs.isCommentWritten == rhs.isCommentWritten
     }
 }
 
