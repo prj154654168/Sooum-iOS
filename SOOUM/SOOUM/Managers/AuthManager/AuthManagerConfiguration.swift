72c9a8bb5041c99db54747764d7f91dcd21d081d
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-01-26 21:08:06 +0900

    [추가] 각 매니저의 옵션을 추가하기 위해 ManagerConfiguartion 추가

---
+++ SOOUM/SOOUM/Managers/AuthManager/AuthManagerConfiguration.swift
@@ -0,0 +1,11 @@
+//
+//  AuthManagerConfiguration.swift
+//  SOOUM
+//
+//  Created by 오현식 on 1/26/25.
+//
+
+import Foundation
+
+
+struct AuthManagerConfiguration: ManagerConfiguration { }
---
+++ SOOUM/SOOUM/Managers/PushManager/PushManagerConfiguration.swift
@@ -0,0 +1,11 @@
+//
+//  PushManagerConfiguration.swift
+//  SOOUM
+//
+//  Created by 오현식 on 1/26/25.
+//
+
+import Foundation
+
+
+struct PushManagerConfiguration: ManagerConfiguration { }
