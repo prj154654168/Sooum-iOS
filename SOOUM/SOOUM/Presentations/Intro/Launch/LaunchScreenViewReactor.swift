b27e30d52f0c85429a8569faf31dea88cc67416a
JDeoks <duginee@naver.com>
2025-01-26 20:27:39 +0900

    [추가] LaunchScreenViewReactor - fetchAppFlag 추가

--- SOOUM/SOOUM/Presentations/Intro/Launch/LaunchScreenViewReactor.swift
+++ SOOUM/SOOUM/Presentations/Intro/Launch/LaunchScreenViewReactor.swift
@@ -13,15 +13,15 @@ import ReactorKit
 /*
  1. 어플 실행
  2. 키체인에 token 정보가 존재하는 지 확인
-    - token 정보가 있다면 메인 홈으로 진입
-    - token 정보가 없다면
-        2.1. RSA 공개 키 요청
-        2.2. deviceID 암호화
-        2.3. 로그인 요청
-            - deviceId가 서버에 등록되어 있다면, (isRegistered == true)
-                2.1.1. 로그인 요청 후 메인 홈으로 진입
-            - deviceId가 서버에 등록되어 있지 않다면, (isRegistered == false)
-                2.1.3. 회원가입 요청 후 메인 홈으로 진입
+ - token 정보가 있다면 메인 홈으로 진입
+ - token 정보가 없다면
+ 2.1. RSA 공개 키 요청
+ 2.2. deviceID 암호화
+ 2.3. 로그인 요청
+ - deviceId가 서버에 등록되어 있다면, (isRegistered == true)
+ 2.1.1. 로그인 요청 후 메인 홈으로 진입
+ - deviceId가 서버에 등록되어 있지 않다면, (isRegistered == false)
+ 2.1.3. 회원가입 요청 후 메인 홈으로 진입
  */
 class LaunchScreenViewReactor: Reactor {
     
@@ -34,12 +34,14 @@ class LaunchScreenViewReactor: Reactor {
     enum Mutation {
         case check(Bool)
         case updateIsRegistered(Bool)
+        case appFlag(Bool)
     }
     
     struct State {
         var mustUpdate: Bool
         /// deviceId 서버 등록 여부, 로그인 성공 여부
         var isRegistered: Bool?
+        var appFlag: Bool?
     }
     
     var initialState: State = .init(
@@ -58,7 +60,10 @@ class LaunchScreenViewReactor: Reactor {
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .launch:
-            return self.check()
+            return .concat([
+                self.fetchAppFlag(),
+                self.check()
+            ])
         }
     }
     
@@ -67,8 +72,12 @@ class LaunchScreenViewReactor: Reactor {
         switch mutation {
         case let .check(mustUpdate):
             newState.mustUpdate = mustUpdate
+            
         case let .updateIsRegistered(isRegistered):
             newState.isRegistered = isRegistered
+            
+        case let .appFlag(appFlag):
+            newState.appFlag = appFlag
         }
         return newState
     }
@@ -97,8 +106,17 @@ extension LaunchScreenViewReactor {
                 }
             }
     }
-}
     
+    private func fetchAppFlag() -> Observable<Mutation> {
+        let request: ConfigureRequest = .appFlag
+        return provider.networkManager.request(Bool.self, request: request)
+            .map { flag in
+                UserDefaults.standard.set(flag, forKey: "AppFlag")
+                return Mutation.appFlag(flag)
+            }
+    }
+}
+
 extension LaunchScreenViewReactor {
     
     func reactorForOnboarding() -> OnboardingViewReactor {
