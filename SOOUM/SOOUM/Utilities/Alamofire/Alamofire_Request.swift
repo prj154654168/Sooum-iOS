0108897d4c9667307e4e1b0f43a7d03405b24c73
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-03-01 16:06:28 +0900

    [추가] 서버 버전이 업데이트 됨에 따라 분기 추가
     - 기존 API와 업데이트 된 API 둘 다 사용 가능

--- SOOUM/SOOUM/Utilities/Alamofire/Alamofire_Request.swift
+++ SOOUM/SOOUM/Utilities/Alamofire/Alamofire_Request.swift
@@ -17,6 +17,12 @@ protocol BaseRequest: URLRequestConvertible {
     var parameters: Parameters { get }
     var encoding: ParameterEncoding { get }
     var authorizationType: AuthorizationType { get }
+    var version: APIVersion { get }
+}
+
+enum APIVersion: String {
+    case v1 = ""
+    case v2 = "/v2"
 }
 
 enum AuthorizationType: String {
