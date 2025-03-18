846bcdca366238c97dc7b3089ed6224aab474b26
JDeoks <duginee@naver.com>
2025-03-18 09:22:43 +0900

    [추가] SOMEvent 주석 추가

--- SOOUM/SOOUM/Managers/GAManager/SOMEvent.swift
+++ SOOUM/SOOUM/Managers/GAManager/SOMEvent.swift
@@ -7,17 +7,20 @@
 
 enum SOMEvent {
   enum WriteCard: FirebaseLoggable {
-    /// 태그를 추가하고 글을 작성하지 않을 때
+    /// 글쓰기 화면에서 태그를 추가하고 글을 작성하지 않음
     case dismiss_with_tag(tag_count: Int, tag_texts: [String])
-    /// 태그를 추가하고 글을 작성할 때
+    /// 글쓰기 화면에서 태그를 추가하고 글을 작성
     case add_tag(tag_count: Int, tag_texts: [String])
   }
   
   enum Tag: FirebaseLoggable {
+    /// 태그를 클릭한 위치
     enum ClickPositionKey {
+      /// 카드 상세화면에서 태그 클릭
       static let post = "post"
-      static let tag_page = "tag_page"
+      /// 추천 태그 목록에서 태그 클릭
       static let recommendation = "recommendation"
+      /// 태그 검색 결과에서 태그 클릭
       static let search_result = "search_result"
     }
     /// 태그를 클릭
@@ -25,6 +28,12 @@ enum SOMEvent {
   }
   
   enum Comment: FirebaseLoggable {
+    /// 사용자가 댓글을 작성
+    ///
+    /// - Parameters:
+    ///  - comment_length: 댓글 길이
+    ///  - parent_post_id: 부모 글 ID
+    ///  - image_attached: 이미지 첨부 여부
     case add_comment(comment_length: Int, parent_post_id: String, image_attached: Bool)
   }
 }
