9d3fa2f6e7c2f33fba236dde0a0bf52c61f81858
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-09-11 14:30:39 +0900

    [변경] 숨 버전2를 위한 SOMButton 업데이트

--- SOOUM/SOOUM/DesignSystem/Components/SOMButton.swift
+++ SOOUM/SOOUM/DesignSystem/Components/SOMButton.swift
@@ -13,7 +13,7 @@ class SOMButton: UIButton {
     var title: String? {
         didSet {
             if oldValue != self.title {
-                self.setConfiguration()
+                self.setNeedsUpdateConfiguration()
             }
         }
     }
@@ -21,7 +21,7 @@ class SOMButton: UIButton {
     var image: UIImage? {
         didSet {
             if oldValue != self.image {
-                self.setConfiguration()
+                self.setNeedsUpdateConfiguration()
             }
         }
     }
@@ -29,7 +29,7 @@ class SOMButton: UIButton {
     var typography: Typography? {
         didSet {
             if oldValue != self.typography {
-                self.setConfiguration()
+                self.setNeedsUpdateConfiguration()
             }
         }
     }
@@ -37,7 +37,7 @@ class SOMButton: UIButton {
     var foregroundColor: UIColor? {
         didSet {
             if oldValue != self.foregroundColor {
-                self.setConfiguration()
+                self.setNeedsUpdateConfiguration()
             }
         }
     }
@@ -45,67 +45,90 @@ class SOMButton: UIButton {
     var hasUnderlined: Bool? {
         didSet {
             if oldValue != self.hasUnderlined {
-                self.setConfiguration()
+                self.setNeedsUpdateConfiguration()
             }
         }
     }
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        self.setupConfiguration()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+private extension SOMButton {
     
-    private func setConfiguration() {
+    func setupConfiguration() {
         
         var configuration = UIButton.Configuration.plain()
+        configuration.contentInsets = .zero
         
-        // 이미지 설정
-        if let image = self.image {
-            configuration.image = image
+        self.configuration = configuration
+        self.backgroundColor = .clear
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        self.configurationUpdateHandler = { [weak self] button in
+            guard let self = self else { return }
             
-            if let foregroundColor = self.foregroundColor {
-                configuration.image?.withTintColor(foregroundColor)
-                configuration.imageColorTransformer = UIConfigurationColorTransformer { _ in
-                    foregroundColor
+            var updatedConfig = button.configuration
+            
+            updatedConfig?.background.backgroundColor = button.isHighlighted ? .som.v2.gray600 : (self.backgroundColor ?? .clear)
+            updatedConfig?.background.backgroundColorTransformer = UIConfigurationColorTransformer { _ in
+                // 비활성화 상태일 때, backgroundColor
+                if button.isEnabled == false {
+                    return .som.v2.gray200
                 }
+                // 하이라이트 상태일 때, backgroundColor
+                if button.isHighlighted {
+                    return self.hasUnderlined == true ? .som.v2.gray100 : .som.v2.gray600
+                }
+                // 기본 상태일 때, backgroundColor
+                return self.backgroundColor ?? .clear
             }
+            
+            self.applyConfiguration(to: &updatedConfig)
+            button.configuration = updatedConfig
+        }
+    }
+    
+    func applyConfiguration(to configuration: inout UIButton.Configuration?) {
+        
+        var foregroundColor: UIColor {
+            return self.isEnabled ? (self.foregroundColor ?? .som.v2.white) : .som.v2.gray400
         }
         
-        // 타이틀 설정
+        if let image = self.image {
+            configuration?.image = image
+            configuration?.imageColorTransformer = UIConfigurationColorTransformer { _ in foregroundColor }
+        }
+        
         if let title = self.title, let typography = self.typography {
             var attributes = typography.attributes
             attributes.updateValue(typography.font, forKey: .font)
-            
-            if let foregroundColor = self.foregroundColor {
-                attributes.updateValue(foregroundColor, forKey: .foregroundColor)
-            }
+            attributes.updateValue(foregroundColor, forKey: .foregroundColor)
             
             if self.hasUnderlined == true {
                 attributes.updateValue(NSUnderlineStyle.single.rawValue, forKey: .underlineStyle)
-                attributes.updateValue(foregroundColor ?? UIColor.som.gray400, forKey: .underlineColor)
+                attributes.updateValue(foregroundColor, forKey: .underlineColor)
+                
+                configuration?.contentInsets = .init(top: 6, leading: 16, bottom: 6, trailing: 16)
             }
             
-            configuration.attributedTitle = .init(title, attributes: AttributeContainer(attributes))
-            configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { _ in
+            configuration?.attributedTitle = .init(title, attributes: AttributeContainer(attributes))
+            configuration?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { _ in
                 AttributeContainer(attributes)
             }
-            
-            if image == nil {
-                configuration.titleAlignment = .center
-            } else {
-                configuration.imagePadding = 2
-            }
         }
         
-        if self.backgroundColor == nil {
-            self.backgroundColor = .clear
-            
-            configuration.contentInsets = .zero
+        if image == nil {
+            configuration?.titleAlignment = .center
+        } else {
+            configuration?.imagePadding = 8
         }
-        
-        self.configuration = configuration
     }
 }
