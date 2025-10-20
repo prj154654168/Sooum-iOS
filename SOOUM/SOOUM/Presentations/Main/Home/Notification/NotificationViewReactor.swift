ce95a20662e936eec37a43e9b191e80bdac8be87
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-10-21 00:40:41 +0900

    [수정] 홈 및 알림 수정

--- SOOUM/SOOUM/Presentations/Main/Home/Notification/NotificationViewReactor.swift
+++ SOOUM/SOOUM/Presentations/Main/Home/Notification/NotificationViewReactor.swift
@@ -153,13 +153,13 @@ class NotificationViewReactor: Reactor {
             case let .activity(activityType):
                 return .concat([
                     self.moreNotification(activityType, with: lastId)
-                        .catch(self.catchClosureNotis)
+                        .catch(self.catchClosureNotisMore)
                 ])
             case .notice:
                 return .concat([
                     self.notificationUseCase.notices(lastId: lastId, size: 10)
                         .map(Mutation.moreNotices)
-                        .catch(self.catchClosureNotices)
+                        .catch(self.catchClosureNoticesMore)
                 ])
             }
         case let .requestRead(selectedId):
@@ -204,10 +204,28 @@ extension NotificationViewReactor {
         }
     }
     
+    var catchClosureNotisMore: ((Error) throws -> Observable<Mutation> ) {
+        return { _ in
+            .concat([
+                .just(.more(unreads: [], reads: [])),
+                .just(.updateIsRefreshing(false))
+            ])
+        }
+    }
+    
     var catchClosureNotices: ((Error) throws -> Observable<Mutation> ) {
         return { _ in
             .concat([
                 .just(.notices([])),
+                .just(.updateIsRefreshing(false))
+            ])
+        }
+    }
+    
+    var catchClosureNoticesMore: ((Error) throws -> Observable<Mutation> ) {
+        return { _ in
+            .concat([
+                .just(.moreNotices([])),
                 .just(.updateIsRefreshing(false))
             ])
         }
