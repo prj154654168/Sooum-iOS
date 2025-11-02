9a19a689339263196983f2dd6872e8e7ad64321b
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-11-02 22:06:28 +0900

    [변경] 이미지 다운 시 key 값이 없다면 url로 사용

--- SOOUM/SOOUM/Extensions/Cocoa/UIImgeView.swift
+++ SOOUM/SOOUM/Extensions/Cocoa/UIImgeView.swift
@@ -16,11 +16,11 @@ extension UIImageView {
 
     static let placeholder: UIImage? = UIColor.som.v2.pMain.toImage
 
-    func setImage(strUrl: String?, with key: String) {
+    func setImage(strUrl: String?, with key: String? = nil) {
         
         if let strUrl: String = strUrl, let url = URL(string: strUrl) {
             /// ImageResource 객체를 생성하여 URL과 Cache Key를 연결
-            let resource = KF.ImageResource(downloadURL: url, cacheKey: key)
+            let resource = KF.ImageResource(downloadURL: url, cacheKey: key ?? strUrl)
             /// Kingfisher에 Resource를 전달하고 모든 캐시/다운로드 로직 위임
             self.kf.setImage(with: resource)
             self.backgroundColor = .clear
