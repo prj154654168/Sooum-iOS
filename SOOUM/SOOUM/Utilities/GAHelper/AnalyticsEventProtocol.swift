1153a4f2fe62df5dfc2ea0b04073df521cc3cff2
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-12-14 21:53:13 +0900

    [변경] 단일 파라미터인 경우에도 처리할 수 있도록 변경

--- SOOUM/SOOUM/Utilities/GAHelper/AnalyticsEventProtocol.swift
+++ SOOUM/SOOUM/Utilities/GAHelper/AnalyticsEventProtocol.swift
@@ -25,29 +25,33 @@ extension AnalyticsEventProtocol {
     
     var parameters: [String: FirebaseLoggable]? {
         let mirror = Mirror(reflecting: self)
-            
-        // Case 1: Associated Value를 가진 Enum Case
-        if let tupleValue = mirror.children.first?.value,
-           Mirror(reflecting: tupleValue).displayStyle == .tuple {
+        
+        guard let child = mirror.children.first else { return nil }
+        
+        // 튜플의 경우
+        if Mirror(reflecting: child.value).displayStyle == .tuple {
             
             var dict = [String: FirebaseLoggable]()
-            let tupleMirror = Mirror(reflecting: tupleValue)
+            let tupleMirror = Mirror(reflecting: child.value)
             
             // 튜플 안의 각 파라미터를 순회
-            for paramChild in tupleMirror.children {
-                guard let paramLabel = paramChild.label else { continue }
-                let paramValue = paramChild.value
-                
+            for tupleChild in tupleMirror.children {
+                guard let paramLabel = tupleChild.label else { continue }
                 // FirebaseLoggable 타입 검사
-                if let loggableValue = paramValue as? any FirebaseLoggable {
+                if let loggableValue = tupleChild.value as? any FirebaseLoggable {
                     dict[paramLabel] = loggableValue
                 }
             }
             return dict.isEmpty ? nil : dict
+        // 단일 파라미터인 경우
+        } else {
             
+            guard let label = child.label else { return nil }
+            // FirebaseLoggable 타입 검사
+            if let loggableValue = child.value as? any FirebaseLoggable {
+                return [label: loggableValue]
+            }
         }
-        // Case 2: Associated Value가 없는 단순 Case (파라미터 없음)
-        // 현재 코드는 단일 파라미터인 경우를 처리하나, Associated Value가 없는 경우는 `nil` 반환이 적절함.
         
         return nil
     }
