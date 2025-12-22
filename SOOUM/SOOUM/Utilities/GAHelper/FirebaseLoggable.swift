8f7ef234803d7adb85393f049a74d12fc1d95375
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-12-22 22:48:19 +0900

    [변경] Firebase analytics 맞춤 이벤트 파라미터 수집

--- SOOUM/SOOUM/Utilities/GAHelper/FirebaseLoggable.swift
+++ SOOUM/SOOUM/Utilities/GAHelper/FirebaseLoggable.swift
@@ -6,11 +6,14 @@
 //
 
 /// Firebase에서 지원하는 데이터 타입만 준수하도록 제한
-protocol FirebaseLoggable {}
+protocol FirebaseLoggable {
+    var description: String { get }
+}
 
-extension String: FirebaseLoggable {}
-extension Int: FirebaseLoggable {}
-extension Double: FirebaseLoggable {}
-extension Bool: FirebaseLoggable {}
+extension String: FirebaseLoggable { var description: String { self } }
+extension Int: FirebaseLoggable { var description: String { "\(self)" } }
+extension Double: FirebaseLoggable { var description: String { "\(self)" } }
+extension Bool: FirebaseLoggable { var description: String { "\(self)" } }
 /// [String]만 허용
 extension Array: FirebaseLoggable where Element == String {}
+extension GAEvent.DetailView.ScreenPath: FirebaseLoggable { var description: String { self.rawValue } }
