dd345d91bbc26486e46bbf3c482b4f863f59c13b
hyeonsik971029 <hyeonsik971029@gmail.com>
2026-01-12 23:11:09 +0900

    Squashed commit of the following:
    commit d664e4a9ec35e2f8eea1fb469bab8686b4ede375
    Author: hyeonsik971029 <hyeonsik971029@gmail.com>
    Date:   Mon Jan 12 22:45:14 2026 +0900
    
        [버전] Develop 2.3.0(203000) 버전 업데이트
    
    commit f86abed6d7ea39ebf42ba315dfb2dbca1dfb8552
    Author: hyeonsik971029 <hyeonsik971029@gmail.com>
    Date:   Mon Jan 12 22:44:46 2026 +0900
    
        [버전] Prodution 2.3.0(203000) 버전 업데이트
    
    commit 1f4633fe334afd2a8b0877cb8d541a1df89d5c67
    Author: hyeonsik971029 <hyeonsik971029@gmail.com>
    Date:   Mon Jan 12 23:10:13 2026 +0900
    
        [수정] 시간 갭 표기 계산법 수정
    
    commit 01a84423a0885eb46dcff3094d52c81455675531
    Author: hyeonsik971029 <hyeonsik971029@gmail.com>
    Date:   Mon Jan 12 22:43:01 2026 +0900
    
        [수정] 일수를 기준으로 시간 표기
    
    commit 8a3596640083ee091a18a512b3f8966bef979ce7
    Author: hyeonsik971029 <hyeonsik971029@gmail.com>
    Date:   Wed Jan 7 17:08:02 2026 +0900
    
        [추가] 화면 중앙에서도 스와이프 제스처 추가

--- SOOUM/SOOUM/Extensions/Foundation/Date.swift
+++ SOOUM/SOOUM/Extensions/Foundation/Date.swift
@@ -32,49 +32,37 @@ extension Date {
     }
     
     func infoReadableTimeTakenFromThis(to: Date) -> String {
-
-        let from: TimeInterval = self.timeIntervalSince1970
-        let to: TimeInterval = to.timeIntervalSince1970
-        let gap: TimeInterval = max(0, to - from)
-
-        let time: Int = .init(gap)
-        let days: Int = time / (24 * 60 * 60)
-        let hours: Int = .init(time % (24 * 60 * 60)) / (60 * 60)
-        let minutes: Int = .init(time % (60 * 60)) / 60
         
-        if days > 368 {
-            return "\(days / 368)년전".trimmingCharacters(in: .whitespaces)
+        // 오늘인 경우에만 시분초 계산
+        let timeInterval = max(0, to.timeIntervalSince(self))
+        let hours = Int(timeInterval) / (60 * 60)
+        
+        if hours < 24 {
+            let minutes = (Int(timeInterval) % (60 * 60)) / 60
+            switch (hours, minutes) {
+            case (1...23, _):   return "\(hours)시간 전"
+            case (0, 11...59):  return "\(minutes / 10)0분 전"
+            case (0, 1...10):   return "\(minutes)분 전"
+            default:            return "방금 전"
+            }
         }
         
-        if days > 29 && days < 369 {
-            return "\(days / 30)개월 전".trimmingCharacters(in: .whitespaces)
-        }
+        let calendar = Calendar.current
+        // 시작 날짜와 끝 날짜의 시분초를 00:00:00으로 초기화하여 날짜 차이만 계산합니다.
+        let startOfFrom = calendar.startOfDay(for: self)
+        let startOfTo = calendar.startOfDay(for: to)
         
-        if days > 6 && days < 30 {
-            return "\(days / 7)주 전".trimmingCharacters(in: .whitespaces)
-        }
+        // 두 날짜 사이의 일수(day) 차이를 구합니다.
+        let components = calendar.dateComponents([.day], from: startOfFrom, to: startOfTo)
+        let days = components.day ?? 0
         
-        if days > 0 && days < 7 {
-            return "\(days)일 전".trimmingCharacters(in: .whitespaces)
+        // 날짜 차이가 1일 이상인 경우 (시분초 무시)
+        switch days {
+        case 365...:    return "\(days / 365)년 전"
+        case 30...:     return "\(days / 30)개월 전"
+        case 7...:      return "\(days / 7)주 전"
+        default:        return "\(days)일 전"
         }
-        
-        if hours > 0 && hours < 24 {
-            return "\(hours)시간 전".trimmingCharacters(in: .whitespaces)
-        }
-        
-        if minutes > 10 && minutes < 60 {
-            return "\(minutes / 10)0분 전".trimmingCharacters(in: .whitespaces)
-        }
-        
-        if minutes > 0 && minutes < 11 {
-            return "\(minutes)분 전".trimmingCharacters(in: .whitespaces)
-        }
-        
-        if minutes < 1 {
-            return "방금 전".trimmingCharacters(in: .whitespaces)
-        }
-
-        return ""
     }
     
     func infoReadableTimeTakenFromThisForPung(to: Date) -> String {
